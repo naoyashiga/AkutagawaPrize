ac5c37e5b4680ca1ce46fe1842a3a880b4243a73
naoyashiga <naoyashiga0@gmail.com>
2015-08-31 21:32:58 +0900

    最初のコミット

---
+++ AkutagawaPrize/ContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerViewController.swift
+//  AkutagawaPrize
+//
+//  Created by naoyashiga on 2015/08/31.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
